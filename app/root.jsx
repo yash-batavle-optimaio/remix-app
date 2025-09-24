6ccf940b1aad4e9f8d82acf3f62d12e9b684d25d
yash-batavle-optimaio <developer@optimaio.com>
2025-09-24 15:08:06 +0530

    partial root.jsx

--- app/root.jsx
+++ app/root.jsx
@@ -12,12 +12,18 @@ export const links = () => [
   { rel: "stylesheet", href: polarisStyles },
 ];
 
+
 export default function App() {
   return (
-    <html lang="en">
+    <html>
       <head>
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width,initial-scale=1" />
+        <link rel="preconnect" href="https://cdn.shopify.com/" />
+        <link
+          rel="stylesheet"
+          href="https://cdn.shopify.com/static/fonts/inter/v4/styles.css"
+        />
         <Meta />
         <Links />
       </head>
