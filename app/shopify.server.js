ae5441520876d64d6ca654e9f0ace20e37500be2
yash-batavle-optimaio <developer@optimaio.com>
2025-09-24 10:21:58 +0530

    env updated

--- app/shopify.server.js
+++ app/shopify.server.js
@@ -6,6 +6,7 @@ import {
 } from "@shopify/shopify-app-remix/server";
 import { PrismaSessionStorage } from "@shopify/shopify-app-session-storage-prisma";
 import prisma from "./db.server";
+import "dotenv/config";
 
 const shopify = shopifyApp({
   apiKey: process.env.SHOPIFY_API_KEY,
