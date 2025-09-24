9630dbe847aa9bce0aa71903a391a254574fb7e7
yash-batavle-optimaio <developer@optimaio.com>
2025-09-24 14:55:11 +0530

    edited app.jsx

--- app/routes/app.jsx
+++ app/routes/app.jsx
@@ -23,6 +23,8 @@ export default function App() {
           Home
         </Link>
         <Link to="/app/additional">Additional page</Link>
+         <Link to="/app/test-api">Additional page</Link>
+                 <a href="/app/test-api">Test Page</a>
       </NavMenu>
       <Outlet />
     </AppProvider>
