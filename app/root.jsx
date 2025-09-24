100c8ecb2acd0da4312705c5039408e9ca4460c3
yash-batavle-optimaio <developer@optimaio.com>
2025-09-24 15:04:04 +0530

    all root.jsx edited

--- app/root.jsx
+++ app/root.jsx
@@ -6,17 +6,18 @@ import {
   ScrollRestoration,
 } from "@remix-run/react";
 
+import polarisStyles from "@shopify/polaris/build/esm/styles.css?url";
+
+export const links = () => [
+  { rel: "stylesheet", href: polarisStyles },
+];
+
 export default function App() {
   return (
-    <html>
+    <html lang="en">
       <head>
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width,initial-scale=1" />
-        <link rel="preconnect" href="https://cdn.shopify.com/" />
-        <link
-          rel="stylesheet"
-          href="https://cdn.shopify.com/static/fonts/inter/v4/styles.css"
-        />
         <Meta />
         <Links />
       </head>
