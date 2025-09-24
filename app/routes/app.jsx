b6e03ecebbdc758e3355e30e27ed581033f576de
yash-batavle-optimaio <developer@optimaio.com>
2025-09-24 15:16:13 +0530

    partial root.jsx app.jsx

--- app/routes/app.jsx
+++ app/routes/app.jsx
@@ -2,14 +2,10 @@ import { Link, Outlet, useLoaderData, useRouteError } from "@remix-run/react";
 import { boundary } from "@shopify/shopify-app-remix/server";
 import { AppProvider } from "@shopify/shopify-app-remix/react";
 import { NavMenu } from "@shopify/app-bridge-react";
-import polarisStyles from "@shopify/polaris/build/esm/styles.css?url";
 import { authenticate } from "../shopify.server";
-
-export const links = () => [{ rel: "stylesheet", href: polarisStyles }];
 
 export const loader = async ({ request }) => {
   await authenticate.admin(request);
-
   return { apiKey: process.env.SHOPIFY_API_KEY || "" };
 };
 
@@ -19,19 +15,15 @@ export default function App() {
   return (
     <AppProvider isEmbeddedApp apiKey={apiKey}>
       <NavMenu>
-        <Link to="/app" rel="home">
-          Home
-        </Link>
-        <Link to="/app/additional">Additional page</Link>
-         <Link to="/app/test-api">Additional page</Link>
-                 <a href="/app/test-api">Test Page</a>
+        <Link to="/app" rel="home">🏠 Home</Link>
+        <Link to="/app/additional">📄 Additional Page</Link>
+        <Link to="/app/test-api">🧪 Test Page</Link>
       </NavMenu>
       <Outlet />
     </AppProvider>
   );
 }
 
-// Shopify needs Remix to catch some thrown responses, so that their headers are included in the response.
 export function ErrorBoundary() {
   return boundary.error(useRouteError());
 }
